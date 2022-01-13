555732a0336e83f588dd3fe9a11cdc7866dd2fc4
Edozie <phynnedo@gmail.com>
2022-01-13 15:23:45 +0100

    Solved the issue with routing

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@ import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <div className="App">
-    //     <Header/>
-    //     <Switch>
-    //       <Route exact path = "/">
-    //         <Main/>
-    //       </Route>
-    //       <Route path = "/result">
-    //         <Result/>
-    //       </Route>
-    //     </Switch>
-    //   </div>
-    // </BrowserRouter>
-
-      <div className="App">
-        <Header/>
-        <Routes>
-          <Route path = "/" element = {<Main/>} exact/>
-          <Route path = "/result" element = {<Result/>} />
-        </Routes>
-      </div>
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route path = "/" element = {<Main/>} exact/>
+        <Route path = "/result" element = {<Result/>} />
+      </Routes>
+    </div>
   );
 }
 
