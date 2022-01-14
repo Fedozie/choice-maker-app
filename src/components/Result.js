dce42932453e3fa3b1856b44701821f7cb36e1e1
Edozie <phynnedo@gmail.com>
2022-01-14 23:18:48 +0100

    Worked on the result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Result = () => {
     return (
-        <div className="main">
-            <h2>Choice Maker App</h2>
+        <div className = "main">
+            <h1>Choice Maker App</h1>
             <p>When the decision is too hard or too simple, randomize the answers with the Choice Maker App</p>
-            <div>          
+            <div className = "result">
+                <div className = "result-answers">
+                    <h1>Placeholder Text!</h1>
+                </div>
+                <div className = "result-buttons">
+                    <button>Ask Again ?</button>
+                    <button>Ask Another Random Question</button>          
+                </div>
             </div>
         </div>
     );
