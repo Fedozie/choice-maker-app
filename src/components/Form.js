d24de04b5b84c5ce303d4a31553a66e5471961e2
Edozie <phynnedo@gmail.com>
2022-03-07 22:19:26 +0100

    Moved the props from Form.js to Main.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,59 +1,27 @@
 import React from 'react';
-import {useState} from "react";
 import {BsPlusCircle} from "react-icons/bs";
 
 const Form = (props) => {
-    const [num, setNum] = useState(3);
     
-    const toABC = (num) => {
-        if(num < 1 || num > 26 || typeof num !== 'number'){
-            return '-1'
-        }
-        const leveller = 64
-        return String.fromCharCode(num + leveller)
-    }
- 
-    const [inputs, setInputs] = useState([
-        {className: 'input-group', type: 'text', name:'option' + toABC(1), placeholder:toABC(1) + ':', id: 1, mainId: 'option' + toABC(1)},
-        {className: 'input-group', type: 'text', name:'option' + toABC(2), placeholder:toABC(2) + ':', id: 2, mainId: 'option' + toABC(2)},
-        {className: 'input-group', type: 'text', name:'option' + toABC(3), placeholder:toABC(3) + ':', id: 3, mainId: 'option' + toABC(3)}
-    ])
-
-    const addOption = (e) => {
-        e.preventDefault();
-        if(num <= 25){
-            setNum(num + 1);
-            setInputs([...inputs, {className: 'input-group', type: 'text', name:'option' + toABC(num+1), placeholder:toABC(num+1) + ':', id:toABC(num+1), mainId: 'option' + toABC(num+1)}]);
-        }else{
-            alert("You have reached your limit, you can't add any more options.")
-        }
-    }
-
-    const [enteredQuestion, setEnteredQuestion] = useState('');
-    const [enteredOptions, setEnteredOptions] = useState([{
-        optionA: '',
-        optionB: '',
-        optionC: ''
-    }]);
     
     return (
         <form className="form" onSubmit={props.onSubmit}>
             <h3 className="headings">Questions</h3>
             <div className="input-group">
-                <input type="text" name="question" id="question" placeholder="Q: Enter your question here" value={enteredQuestion} onChange={(e) => setEnteredQuestion(e.target.value)}/>
+                <input type="text" name="question" id="question" placeholder="Q: Enter your question here" value={props.enteredQuestion} onChange={(e) => props.setEnteredQuestion(e.target.value)}/>
             </div>
             <hr/>
             <h3 className="headings">Options</h3>
             <div className="options">
                 {
-                    inputs.map((input) => (
+                    props.inputs.map((input) => (
                         <div className={input.className} key={input.id}>
-                            <input type={input.type} name={input.name} id={input.mainId} placeholder={input.placeholder} value={enteredOptions.option} onChange={(e) => setEnteredOptions(e.target.value)} />
+                            <input type={input.type} name={input.name} id={input.mainId} placeholder={input.placeholder} value={props.enteredOptions.option} onChange={(e) => props.setEnteredOptions(e.target.value)} />
                         </div>
                     ))
                 }
                 <div className="buttons">
-                    <button className="optionButton" onClick={addOption}> 
+                    <button className="optionButton" onClick={props.addOption}> 
                         <BsPlusCircle className = "plus-icon"/> 
                         Add Another Option
                     </button>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,38 @@ import {useState} from "react";
 import Form from './Form';
 
 const Main = (props) => {
+    const [num, setNum] = useState(3);
     
+    const toABC = (num) => {
+        if(num < 1 || num > 26 || typeof num !== 'number'){
+            return '-1'
+        }
+        const leveller = 64
+        return String.fromCharCode(num + leveller)
+    }
+ 
+    const [inputs, setInputs] = useState([
+        {className: 'input-group', type: 'text', name:'option' + toABC(1), placeholder:toABC(1) + ':', id: 1, mainId: 'option' + toABC(1)},
+        {className: 'input-group', type: 'text', name:'option' + toABC(2), placeholder:toABC(2) + ':', id: 2, mainId: 'option' + toABC(2)},
+        {className: 'input-group', type: 'text', name:'option' + toABC(3), placeholder:toABC(3) + ':', id: 3, mainId: 'option' + toABC(3)}
+    ])
+
+    const addOption = (e) => {
+        e.preventDefault();
+        if(num <= 25){
+            setNum(num + 1);
+            setInputs([...inputs, {className: 'input-group', type: 'text', name:'option' + toABC(num+1), placeholder:toABC(num+1) + ':', id:toABC(num+1), mainId: 'option' + toABC(num+1)}]);
+        }else{
+            alert("You have reached your limit, you can't add any more options.")
+        }
+    }
+
+    const [enteredQuestion, setEnteredQuestion] = useState('');
+    const [enteredOptions, setEnteredOptions] = useState([{
+        optionA: '',
+        optionB: '',
+        optionC: ''
+    }]);
 
     return (
         <div className="main">
@@ -11,7 +42,16 @@ const Main = (props) => {
                 <h2>Choice Maker App</h2>
                 <p>When the decision is too hard or too simple, randomize the answers with the Choice Maker App</p>
             </div>
-            <Form onSubmit = {props.toResult}/>
+            <Form 
+                onSubmit = {props.toResult}
+                inputs = {inputs}
+                setInputs = {setInputs}
+                addOption = {addOption}
+                enteredQuestion = {enteredQuestion}
+                setEnteredQuestion = {setEnteredQuestion}
+                enteredOptions = {enteredOptions}
+                setEnteredOptions = {setEnteredOptions}
+            />
         </div>
     );
 }
