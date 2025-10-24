63f33fe4fc01eea8d453bd9df50ffab59ec9f197
Edozie Onyeanusi <phynnedo@gmail.com>
2025-10-24 09:41:10 +0100

    feat: initial commit for refactoring

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Result from "./components/Result";
+import { Route, Routes } from 'react-router';
 
 function App() {
-  const [display, setDisplay] = useState(true);
+  // const [display, setDisplay] = useState(true);
 
-  const startHandler = () => {
-    setDisplay(false);
-  }
+  // const startHandler = () => {
+  //   setDisplay(false);
+  // }
   
-  const endHandlder = () => {
-    setDisplay(true);
-  }
+  // const endHandlder = () => {
+  //   setDisplay(true);
+  // }
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    startHandler();
-  }
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   startHandler();
+  // }
 
   return (
     <div className="App">
       <Header/>
-      {display && <Main toResult = {submitHandler}/>}
-      {!display && <Result toForm = {endHandlder}/>}
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/result' element={<Result/>}/>
+      </Routes>
     </div>
   );
 }
